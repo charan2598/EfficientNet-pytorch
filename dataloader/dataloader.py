ebef788277950aa809ea0e0b2365eadac3c3cbad
Charan Kumar <charan2598@gmail.com>
2025-02-16 23:39:35 -0800

    Added dataloader.py file.

---
+++ dataloader/dataloader.py
@@ -0,0 +1,6 @@
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils.data.dataset import Dataset
+
+# Create dataset and dataloaders for CIFAR 10 and CIFAR 100
